df876e8186fec80c115d0d72c1bf49545a4ce120
shubhi <shubhipun@gmail.com>
2020-04-28 14:52:38 +0530

    move private info into dev.js

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./models/user");
+const config = require("./config/key");
 
 mongoose
-  .connect(
-    "mongodb+srv://Shubhi:Shubhi@react-1-nwvdm.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("DB Connected!"))
   .catch((err) => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.json({ "hello ~": "Hi ~~dsad" });
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true, err });
+  });
 });
 
 app.listen(5000, () => {
